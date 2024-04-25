ca33d04b744ac5fc48a236da0e094d080173e549
Mehmet Eren Çiftçi <64750696+amacsizadam4@users.noreply.github.com>
2024-04-25 14:03:35 +0300

    Methodlar oluşturuldu

--- NTPkulekontrol/src/Main.java
+++ NTPkulekontrol/src/Main.java
@@ -1,8 +1,36 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    //global variables
+    public static int isAdmin;
+
     public static void main(String[] args) {
         // Alt + Enter for suggestions
-        System.out.printf("Test");
+
     }
+
+
+    public static void loginScreen() {
+        System.out.println("1 - Giriş Yap");
+        System.out.println("2 - Kayıt Ol");
+        System.out.println("3 - Ayarlar");
+        System.out.println("4 - Programı Kapat");
+    }
+
+    public static void mainMenu() {
+        if (isAdmin==1) {
+            admin();
+        }
+        else customer();
+
+    }
+
+    public static void customer() {
+
+    }
+    public static void admin() {
+        System.out.println();
+    }
+
 }
