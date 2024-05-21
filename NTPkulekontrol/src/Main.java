407d4c4db1ee478571c49b949123bad4e594c116
Mehmet Eren Çiftçi <64750696+amacsizadam4@users.noreply.github.com>
2024-05-21 13:35:56 +0300

    fixed Turkish

--- NTPkulekontrol/src/Main.java
+++ NTPkulekontrol/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         // ÖNEMLİ!
+        System.setProperty("file.encoding", "UTF-8");
         loginScreen();
 
     }
@@ -26,7 +27,6 @@ public class Main {
         System.out.println("═════════════════════════════════════════════");
         System.out.println("1 - Giriş Yap");
         System.out.println("2 - Kayıt Ol");
-        System.out.println("3 - Ayarlar");
         System.out.println("4 - Programı Kapat");
         System.out.println("═════════════════════════════════════════════");
         System.out.println("Seçiminizi yapınız: ");
